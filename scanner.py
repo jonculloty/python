3d15e49f531d6eca27b13d59d5401cd5a3abcbd0
culljon <culljon@horse.(none)>
2013-01-15 23:50:16 +0000

    adding location

--- scanner.py
+++ scanner.py
@@ -7,23 +7,44 @@
 # This script finds all these entries and counts SYN attempts per IP
 #
 
-# collaborate :)
-
 import re
+import urllib2
+from xml.dom import minidom
 
-ips = {}
-pattern = re.compile('SRC=\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}')
+class Scanner:
 
-f = open('/var/log/kern.log', 'r')
-for line in f:
-    if "HACKER" in line: 
-        ipl = pattern.findall(line)
-        ipstr = ipl[0].split("=")[1]
+    """ Check Ubuntu logs for strange things """
 
-        if ipstr in ips:
-            ips[ipstr] = ips[ipstr] + 1
-        else:
-            ips[ipstr]=1
+    def CheckBruteForce(self):
+        pattern = re.compile('SRC=\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}')
+        ips = {}
+        f = open('/var/log/kern.log', 'r')
+        for line in f:
+            if "HACKER" in line: 
+                ipl = pattern.findall(line)
+                ipstr = ipl[0].split("=")[1]
 
-for key, value in ips.items():
-    print "%s (%s)" % (key,value)
+                if ipstr in ips:
+                    ips[ipstr] = ips[ipstr] + 1
+                else:
+                    ips[ipstr]=1
+        return ips
+
+    def GetLocation(self, ip):
+        url = "http://www.geoplugin.net/xml.gp?ip=%s" % ip
+        usock = urllib2.urlopen(url) 
+        xmldoc = minidom.parse(usock)
+
+        for element in xmldoc.getElementsByTagName('geoplugin_countryName'):
+            return element.firstChild.nodeValue
+
+    def PrintReport(self, ips):
+        for key, value in ips.items():
+            country = self.GetLocation(key)
+            print "%-20s %-5s %s" % (key,value,country)
+
+if __name__ == "__main__":
+
+    x = Scanner()
+    susIPs = x.CheckBruteForce()
+    x.PrintReport(susIPs)
